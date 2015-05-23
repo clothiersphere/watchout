d8b19cd2fbaa79b93e6a1bc029553556a7c33ab7
spacemixup <xchrislee@gmail.com>
2015-05-22 19:36:55 -0700

    Collision working correctly now.

--- watchout.js
+++ watchout.js
@@ -6,13 +6,26 @@ var currentScore = 0;
 var scoreBoard = d3.select(".scoreboard");
 var height = window.innerHeight
 var width = window.innerWidth
-var nEnemies = 1;
+var nEnemies = 25;
+var collisionE={};
 // var height = window.innerHeight - d3.select('.scoreboard').style('height');
 var gameboard = d3.select("body").append("svg:svg").attr("width", width).attr("height", height);
 //var path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
 ////  SCORE //////
 
-
+var updateScore = function(){
+  d3.select(".collisions span").text(collisionCount);
+  //console.log(collisionCount + " outside");
+  if(!highScore || currentScore > highScore){
+    highScore = currentScore;
+  }
+  currentScore = 0;
+  //collisionCount = 0;
+  d3.select(".current span").text(currentScore);
+  d3.select(".high span").text(highScore);
+  //d3.select(".collisions span").text(collisionCount);
+  //console.log(collisionCount + " inside");
+}
 
 
 //// Player class /////
@@ -28,7 +41,6 @@ Player.prototype.init = function(x,y){
 }
 
 Player.prototype.moveRelative = function(dx, dy){
-  console.log("in move relative");
   this.setPosition(this.x + dx, this.y + dy);
 }
 
@@ -48,9 +60,10 @@ Player.prototype.createPlayer = function(){
 Player.prototype.setPosition = function(x,y){
   this.x = x;
   this.y = y;
-  this.r = 20;
-  this.circle.attr("cx", x).attr("cy", y).attr("r", 20);
+  this.r = 40;
+  this.circle.attr("cx", x).attr("cy", y).attr("r", this.r);
 }
+
 
 var player = new Player();
 
@@ -71,25 +84,22 @@ var appendEnemies = function(enemy_array){
   enemies.enter().append('svg:circle').attr('class', 'enemy')
       .attr('cx', function(enemy) { return enemy.x })
       .attr('cy', function(enemy) { return enemy.y })
-      .attr('r', 7).attr('fill','#FF66FF');
+      .attr('r', 14).attr('fill','#FF66FF');
 }
 
 var moveEnemies = function(){
   return function(){
     var enemy_array = createEnemies(nEnemies);
-    gameboard.selectAll('.enemy').data(enemy_array, function(d){ return d.id }).transition().duration(2000)
-    .tween("custom", function(){
-
+    gameboard.selectAll('.enemy').data(enemy_array, function(d){ return d.id }).transition().duration(1500)
+    .attr("cx", function(enemy) { return enemy.x }).attr("cy", function(enemy) { return enemy.y }).tween("custom", function(){
       return function(t) {
         var enemy = d3.select(this);
-        enemy.attr("cx", function(enemy) { return enemy.x }).attr("cy", function(enemy) { return enemy.y })
         tweenWithCollisionDetection(enemy);
-        collisionCount++;
       }
     });
 
 
-    d3.timer(moveEnemies(), 2000);
+    d3.timer(moveEnemies(), 1500);
     return true;
   }
 }
@@ -109,11 +119,22 @@ var tweenWithCollisionDetection = function(enemy){
   separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
 
   //debugger;
-  if (separation < radiusSum) {
-    console.log("collision!");
-    //updateScore();
+  if(separation >= radiusSum){
+      collisionE[enemy.data()[0].id] = true;
+  }
+  if (separation < radiusSum ) {
+    if(collisionE[enemy.data()[0].id]){
+      collisionE[enemy.data()[0].id] = false;
+      collisionCount++;
+      updateScore();
+    }
   }
 }
+
+setInterval(function(){
+  currentScore++;
+  d3.select(".current span").text(currentScore);
+}, 1000)
 
 
 /// collision detection ////
